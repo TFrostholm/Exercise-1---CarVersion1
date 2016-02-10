09da0ef920544fa8b2a17884dd6215512fad0c06
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-10 14:30:08 +0100

    Initial commit

---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            // Declaration of some variables representing properties of a car:
+            String brand;
+            String modelName;
+            String color;
+            int numberOfCylinders;
+            int horsePower;
+            int numberOfSeats;
+            double engineSize;
+            double kilometersPerLiter;
+            Boolean isDiesel;
+
+
+            // YOUR TASK: Assign some proper values to the variables
+            // Do this below this line
+
+            brand = "Mercedes";
+            modelName = "c320";
+            color = "Black";
+            numberOfCylinders = 6;
+            horsePower = 430;
+            numberOfCylinders = 4;
+            engineSize = 4.4;
+            kilometersPerLiter = 12;
+            isDiesel = false;
+
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
